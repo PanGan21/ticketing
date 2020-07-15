d6add475251dd0f766772a2a33ae46ca9467148c
Panagiotis Ganelis <panagiotisganelis@Panagiotiss-MacBook-Pro.local>
2020-07-15 13:41:14 +0200

    test signup invalid email status code

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -8,3 +8,10 @@ it("return a 201 on successful signup", async () => {
     .send({ email: "test@test.com", password: "password" })
     .expect(201);
 });
+
+it("returns a 400 with an invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({ email: "testtestcom", password: "password" })
+    .expect(400);
+});
