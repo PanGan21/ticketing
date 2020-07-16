24d7e7b0eefd1e95f80fc2e28bcc5761f7121745
PanGan21 <panagiotisganelis@Panagiotiss-MacBook-Pro.local>
2020-07-17 00:30:03 +0200

    fix the imports in auth

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,3 +1,4 @@
+// re export the fuctions under `common` path
 export * from "./errors/bad-request-error";
 export * from "./errors/custom-error";
 export * from "./errors/database-connection-error";
