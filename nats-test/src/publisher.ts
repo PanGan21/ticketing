75fc73bdcac4fc273bc22e1e8a0c8327ee0e7f1e
PanGan21 <panagiotisganelis@Panagiotiss-MacBook-Pro.local>
2020-07-20 17:40:11 +0200

    make the publish function async in order to be able to await

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -5,22 +5,18 @@ console.clear();
 
 const stan = nats.connect("ticketing", "abc", { url: "http://localhost:4222" });
 
-stan.on("connect", () => {
+stan.on("connect", async () => {
   console.log("Publisher connected to NATS");
 
-  new TicketCreatedPublisher(stan).publish({
-    id: "123",
-    title: "concert",
-    price: 20,
-  });
+  const publisher = new TicketCreatedPublisher(stan);
 
-  //   const data = JSON.stringify({
-  //     id: "123",
-  //     title: "concert",
-  //     price: 20,
-  //   });
-
-  //   stan.publish("ticket:created", data, () => {
-  //     console.log("Event published");
-  //   });
+  try {
+    await publisher.publish({
+      id: "123",
+      title: "concert",
+      price: 20,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 });
