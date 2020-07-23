2872ee4ecafd676bba690bf178c8c95cd1d879c2
PanGan21 <panagiotisganelis@Panagiotiss-MacBook-Pro.local>
2020-07-23 19:56:04 +0200

    create new order route without publishing event

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -12,6 +12,8 @@ import { Order } from "../models/order";
 
 const router = express.Router();
 
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
 router.post(
   "/api/orders",
   requireAuth,
@@ -27,19 +29,28 @@ router.post(
     }
 
     // Make sure that this ticket is not already reserved
-    // Means that a ticket is not associated with an order and the order's status is not cancelled
-    // Run a query to look at all orders. Find an order where the ticket is the one found and the status is not cancelled
     const isReserved = await ticket.isReserved();
     if (isReserved) {
       throw new BadRequestError("Ticket is already reserved");
     }
 
     // Calculate an expiration date for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
 
     // Build the order and save it to the database
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+
+    await order.save();
 
     // Publish an event saying that an order was created
-    res.send({});
+
+    res.status(201).send(order);
   }
 );
 
