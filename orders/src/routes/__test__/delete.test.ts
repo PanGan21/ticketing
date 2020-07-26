3e8b95500c6d3502d6cbfe7de240c85c917e8f4d
PanGan21 <panagiotisganelis@Panagiotiss-MacBook-Pro.local>
2020-07-26 20:53:09 +0200

    publish events from the routes and unit test them

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -2,6 +2,7 @@ import request from "supertest";
 import { Ticket } from "../../models/ticket";
 import { app } from "../../app";
 import { OrderStatus } from "@pgtickets/common";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("marks an order as cancelled", async () => {
   // Create a ticket with Ticket Model
@@ -36,4 +37,29 @@ it("marks an order as cancelled", async () => {
   expect(updatedOrder.status).toEqual(OrderStatus.Cancelled);
 });
 
-it.todo("emits an order cancelled event");
+it("emits an order cancelled event", async () => {
+  // Create a ticket with Ticket Model
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+  // Make a request to create an order
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // Make a request to cancel an order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(204);
+
+  // Check if event has been published
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -6,6 +6,9 @@ import {
   OrderStatus,
 } from "@pgtickets/common";
 import { Order } from "../models/order";
+import { OrderCancelledPublisher } from "../events/publishers/order-cancelled-publisher";
+import { natsWrapper } from "../nats-wrapper";
+import { Ticket } from "../models/ticket";
 
 const router = express.Router();
 
@@ -15,7 +18,7 @@ router.delete(
   async (req: Request, res: Response) => {
     const { orderId } = req.params;
 
-    const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId).populate("ticket");
 
     if (!order) {
       throw new NotFoundError();
@@ -28,6 +31,12 @@ router.delete(
     order.save();
 
     // publising an event saying this was cancelled
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
 
     res.status(204).send(order);
   }
