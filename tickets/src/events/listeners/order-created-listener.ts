be5f11794e79705d946b3695b1bcc33fd883c4b9
PanGan21 <panagiotisganelis@Panagiotiss-MacBook-Pro.local>
2020-07-28 22:09:51 +0200

    order created event listener in tickets service

---
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,11 @@
+import { Listener, OrderCreatedEvent, Subjects } from "@pgtickets/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+  onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    throw new Error("Method not implemented.");
+  }
+}
