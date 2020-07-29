ca4dadda4b4c25f90903c58d140d0ca392cd64b3
PanGan21 <panagiotisganelis@Panagiotiss-MacBook-Pro.local>
2020-07-29 10:14:20 +0200

    order created event listener in tickets service

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -5,7 +5,5 @@ import { queueGroupName } from "./queue-group-name";
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
-  onMessage(data: OrderCreatedEvent["data"], msg: Message) {
-    throw new Error("Method not implemented.");
-  }
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
 }
