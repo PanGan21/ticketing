f065aeca88732ebdcae576461d1aa59a5cb6e308
PanGan21 <panagiotisganelis@Panagiotiss-MacBook-Pro.local>
2020-07-29 12:03:34 +0200

    fix the clear all mocks in test setup of tickets service

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -15,7 +15,6 @@ jest.mock("../nats-wrapper");
 
 let mongo: any;
 beforeAll(async () => {
-  jest.clearAllMocks();
   process.env.JWT_KEY = "asdfasdf";
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
@@ -29,6 +28,7 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
+  jest.clearAllMocks();
   const collections = await mongoose.connection.db.collections();
 
   for (let collection of collections) {
